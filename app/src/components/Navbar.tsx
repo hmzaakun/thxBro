c147e561e793e100aa2574e7c39984fc943aa72e
hmzaakun <hamza.mekhaneg@icloud.com>
2024-07-13 18:40:57 +0200

    logo update

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
 
-import iconCard from "/public/card.svg";
+import iconLogo from "/public/logo.png";
 
 const Navbar = () => {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center bg-background border-b">
         <Link href="/" className="flex items-center justify-center" prefetch={false}>
-          <Image src={iconCard} alt="card icon"  className="h-6 w-6 text-primary" />
+          <Image src={iconLogo} alt="card icon"  className="h-14 w-14 text-primary" />
           <span className="sr-only">ThxBro</span>
           <h4 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem] text-primary-foreground">
           ThxBro
